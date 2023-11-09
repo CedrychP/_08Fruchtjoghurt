07dca43c8cee57d7e779c4f285f89730d5d3af21
Philipp Cedrych <philipp.cedrych@compax.at>
2023-11-09 14:26:46 +0100

    Implementation

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,38 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Eingabe with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+  public static void main(String[] args) {
 
-        // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    double kcalFruchtjoghurt = 96;
+    double fettFruchtjoghurt = 3;
+    double eiweissFruchtjoghurt = 3;
+    double preisFruchtjoghurt = 0.3;
 
-            // Press Umschalt+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Strg+F8.
-            System.out.println("i = " + i);
-        }
+    double kcalLightJoghurt = 60;
+    double fettLightJoghurt = 1;
+    double eiweissLightJoghurt = 4;
+    double preisLightJoghurt = 0.5;
+
+    double maxKcalProTag = 3000;
+    double maxFettProTag = 90;
+    double maxAusgabenProTag = 15;
+
+    double maxFruchtjoghurtMenge = maxAusgabenProTag / preisFruchtjoghurt; // in Bechern
+    double maxLightJoghurtMenge = maxAusgabenProTag / preisLightJoghurt; // in Bechern
+
+    double maxKcalFruchtjoghurt = maxKcalProTag / kcalFruchtjoghurt * 100; // in Gramm
+    double maxFettFruchtjoghurt = maxFettProTag / (fettFruchtjoghurt / 100); // in Gramm
+
+    double maxKcalLightJoghurt = maxKcalProTag / kcalLightJoghurt * 100; // in Gramm
+    double maxFettLightJoghurt = maxFettProTag / (fettLightJoghurt / 100); // in Gramm
+
+    double maxEiweissFruchtjoghurt = maxKcalFruchtjoghurt / (kcalFruchtjoghurt / 100) * eiweissFruchtjoghurt / 100; // in Gramm
+    double maxEiweissLightJoghurt = maxKcalLightJoghurt / (kcalLightJoghurt / 100) * eiweissLightJoghurt / 100; // in Gramm
+
+    double empfohleneMengeFruchtjoghurt = Math.min(Math.min(maxFruchtjoghurtMenge, maxKcalFruchtjoghurt), Math.min(maxFettFruchtjoghurt, maxEiweissFruchtjoghurt));
+    double empfohleneMengeLightJoghurt = Math.min(Math.min(maxLightJoghurtMenge, maxKcalLightJoghurt), Math.min(maxFettLightJoghurt, maxEiweissLightJoghurt));
+
+    System.out.println("Empfohlene Tägliche Menge Fruchtjoghurt: " + empfohleneMengeFruchtjoghurt + " Gramm");
+    System.out.println("Empfohlene Tägliche Menge Light-Joghurt: " + empfohleneMengeLightJoghurt + " Gramm");
     }
-}
+  }
